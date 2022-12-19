5ffcdea37e988d851ab939c27965b66fc0c64290
aisuneko <iceneko@protonmail.ch>
2022-12-20 00:14:45 +0800

    delete menu

--- app/src/main/java/me/aisuneko/httpshare/MainActivity.kt
+++ app/src/main/java/me/aisuneko/httpshare/MainActivity.kt
@@ -2,8 +2,6 @@ package me.aisuneko.httpshare
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -11,8 +9,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_first.*
-import kotlinx.android.synthetic.main.activity_main.fab
 import me.aisuneko.httpshare.databinding.ActivityMainBinding
 import java.io.IOException
 
@@ -71,21 +69,6 @@ class MainActivity : AppCompatActivity() {
         closeServer()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
